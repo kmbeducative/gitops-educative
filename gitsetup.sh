c2d74d1c56bf6689c93f715781cade689ec56c1f
kmb385 <kmb385@gmail.com>
2022-07-04 11:36:05 -0400

    Updated

--- gitsetup.sh
+++ gitsetup.sh
@@ -6,7 +6,8 @@ git config --global user.email "student@educative.com"
 git config --global user.name "Educative Student"
 echo "https://{{GITHUB_USERNAME}}:{{GITHUB_PAT}}@github.com" >> .gitcredentials
 git init
-git checkout -m main
+git branch -m main
 git add .
 git commit -m "Lesson started"
-gh repo create --public -s .
+gh repo delete usercode
+gh repo create usercode --public -s $(pwd)
