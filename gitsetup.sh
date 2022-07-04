8584ac9b13d524cdd8240cbc9bac86328be53ae5
kmb385 <kmb385@gmail.com>
2022-07-04 16:18:41 -0400

    :Revert "Updated"
    This reverts commit d889c077d5c13894695e834c9e70581beb26a483.

--- gitsetup.sh
+++ gitsetup.sh
@@ -1,15 +1,11 @@
 
 cd /usercode
 mv conf .gitignore
-export GITHUB_TOKEN={{GITHUB_PAT}}
-export GITHUB_USER={{GITHUB_USERNAME}}
 export GH_TOKEN={{GITHUB_PAT}}
-
 git config --global credential.helper store
 git config --global user.email "student@educative.com"
 git config --global user.name "Educative Student"
 echo "https://{{GITHUB_USERNAME}}:{{GITHUB_PAT}}@github.com" >> ~/.git-credentials
-
 git init
 git add .gitignore
 git commit -m "Lesson started"
@@ -19,12 +15,3 @@ git branch -m main
 
 gh repo delete usercode --confirm
 gh repo create usercode --public -s $(pwd) --push
-
-flux bootstrap github \
-  --owner=$GITHUB_USER \
-  --repository=flux-infra \
-  --branch=main \
-  --path=./clusters/educative-cluster \
-  --personal
-
-  git clone https://github.com/$GITHUB_USER/flux-infra
