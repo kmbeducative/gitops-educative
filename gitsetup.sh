70a3f5abefd72077a1827f38a3fdf8c19ba38b94
kmb385 <kmb385@gmail.com>
2022-07-04 12:13:43 -0400

    Updated

--- gitsetup.sh
+++ gitsetup.sh
@@ -5,7 +5,7 @@ export GH_TOKEN={{GITHUB_PAT}}
 git config --global credential.helper store
 git config --global user.email "student@educative.com"
 git config --global user.name "Educative Student"
-echo "https://{{GITHUB_USERNAME}}:{{GITHUB_PAT}}@github.com" >> .gitcredentials
+echo "https://{{GITHUB_USERNAME}}:{{GITHUB_PAT}}@github.com" >> ~/.gitcredentials
 git init
 git branch -m main
 git add .gitignore
@@ -13,6 +13,5 @@ git commit -m "Lesson started"
 git add .
 git commit -m "Remaining lesson files"
 
-gh auth login
 gh repo delete usercode --confirm
 gh repo create usercode --public -s $(pwd) --push
