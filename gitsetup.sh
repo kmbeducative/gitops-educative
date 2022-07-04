53c0866707083caa64b30af76644766525d25239
kmb385 <kmb385@gmail.com>
2022-07-04 11:25:43 -0400

    Updated

--- gitsetup.sh
+++ gitsetup.sh
@@ -1,11 +1,12 @@
 
 cd /usercode
-GH_TOKEN={{GITHUB_PAT}}
+export GH_TOKEN={{GITHUB_PAT}}
 git config --global credential.helper store
 git config --global user.email "student@educative.com"
 git config --global user.name "Educative Student"
 echo "https://{{GITHUB_USERNAME}}:{{GITHUB_PAT}}@github.com" >> .gitcredentials
 git init
+git checkout -m main
 git add .
 git commit -m "Lesson started"
 gh repo create --public -s .
