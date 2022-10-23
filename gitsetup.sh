493962a9502e5620857673eee950e7ea90a50c6b
kmb385 <kmb385@gmail.com>
2022-10-23 06:16:19 -0400

    update

--- gitsetup.sh
+++ gitsetup.sh
@@ -7,8 +7,10 @@ kubectl apply -f service.yaml
 
 EXTERNAL_IP=$(kubectl get service ingress-nginx-controller -n ingress-nginx -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-sed -i 's/${INGRESS_EXTERNAL_IP}/$EXTERNAL_IP/' ingress.yaml
-sed -i 's/${INGRESS_EXTERNAL_IP}/$EXTERNAL_IP/' canary.yaml
+echo $EXTERNAL_IP
+
+sed -i "s/INGRESS_EXTERNAL_IP/$EXTERNAL_IP/" ingress.yaml
+sed -i "s/INGRESS_EXTERNAL_IP/$EXTERNAL_IP/" canary.yaml
 
 kubectl apply -f ingress.yaml
 kubectl apply -f canary.yaml
